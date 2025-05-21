892df466360d22bf406fa051889c58fbd05ba08f
maruthi0209 <sethumaruthi93@gmail.com>
2025-05-21 16:47:30 +0530

    modified getToken with .then instead of async await

--- src/Components/GetUserIdFromEmail.jsx
+++ src/Components/GetUserIdFromEmail.jsx
@@ -1,20 +1,26 @@
-const {jwtDecode} = await import('jwt-decode')
+const { jwtDecode } = await import('jwt-decode')
 
-async function getToken() {
-    try {
-        const decoded = jwtDecode(token);
-        const response = await fetch("https://fullstackproject-backend-z5rx.onrender.com/users/getidfromemail/" + `${(decoded.email)? decoded.email : decoded.userEmail}`)
-        if(!response.ok) {
-            throw new Error("Error occured " + response.json())
-        }
-        const jsonResponse = await response.json()
-        return jsonResponse
-    } catch (error) {
-        console.error('Invalid token:', error);
-    }
+function getToken() {
+    // try {
+    //     const decoded = jwtDecode(token);
+    //     const response = await fetch("https://fullstackproject-backend-z5rx.onrender.com/users/getidfromemail/" + `${(decoded.email)? decoded.email : decoded.userEmail}`)
+    //     if(!response.ok) {
+    //         throw new Error("Error occured " + response.json())
+    //     }
+    //     const jsonResponse = await response.json()
+    //     return jsonResponse
+    // } catch (error) {
+    //     console.error('Invalid token:', error);
+    // }
+    const decoded = jwtDecode(token);
+    fetch("https://fullstackproject-backend-z5rx.onrender.com/users/getidfromemail/" + `${(decoded.email) ? decoded.email : decoded.userEmail}`)
+        .then((response) => {
+            if (!response.ok) { throw new Error("Error occurred " + response.status); }
+            return response.json();
+        }).then((jsonResponse) => { return jsonResponse }).catch((error) => { console.log(error) })
 }
 
-export default function decodeToken(){
+export default function decodeToken() {
     const token = localStorage.getItem('userToken');
     if (token) {
         getToken(token)
