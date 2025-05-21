211bf96c2280c7923e8f2a5dbeb714dc659df708
maruthi0209 <sethumaruthi93@gmail.com>
2025-05-21 16:38:57 +0530

    modified function for decode Token

--- src/Components/GetUserIdFromEmail.jsx
+++ src/Components/GetUserIdFromEmail.jsx
@@ -1,20 +1,24 @@
 const {jwtDecode} = await import('jwt-decode')
 
-export default async function decodeToken(){
-            const token = localStorage.getItem('userToken');
-            if (token) {
-            try {
-                const decoded = jwtDecode(token);
-                const response = await fetch("https://fullstackproject-backend-z5rx.onrender.com/users/getidfromemail/" + `${(decoded.email)? decoded.email : decoded.userEmail}`)
-                if(!response.ok) {
-                    throw new Error("Error occured " + response.json())
-                }
-                const jsonResponse = await response.json()
-                return jsonResponse
-            } catch (error) {
-                console.error('Invalid token:', error);
-            }
-            } else {
-                return null;
-            }
+async function getToken() {
+    try {
+        const decoded = jwtDecode(token);
+        const response = await fetch("https://fullstackproject-backend-z5rx.onrender.com/users/getidfromemail/" + `${(decoded.email)? decoded.email : decoded.userEmail}`)
+        if(!response.ok) {
+            throw new Error("Error occured " + response.json())
         }
+        const jsonResponse = await response.json()
+        return jsonResponse
+    } catch (error) {
+        console.error('Invalid token:', error);
+    }
+}
+
+export default function decodeToken(){
+    const token = localStorage.getItem('userToken');
+    if (token) {
+        getToken(token)
+    } else {
+        return null;
+    }
+}
