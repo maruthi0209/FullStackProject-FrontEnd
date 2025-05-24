1beb2add3beff6a91775bc0eabb7a63b24a2d050
maruthi0209 <sethumaruthi93@gmail.com>
2025-05-24 16:25:58 +0530

    modified loader icon

--- src/Components/Util/Loader.jsx
+++ src/Components/Util/Loader.jsx
@@ -1,14 +1,15 @@
 // components/Loader.jsx
 import React from 'react';
-// import './Loader.css'; // Add your styles
-import imgURL from "../../assets/MovieProduction.gif"
+import '../../App.css'; // Add your styles
+// import imgURL from "../../assets/MovieProduction.gif"
 
 const Loader = () => {
 
   return (
-    <div className="loader-container">
-      {/* <div className="loader"></div> */}
-      <img src={imgURL} alt="Loading..." />
+    <div className="loader-container m-auto">
+      <div className="loader"></div>
+      {/* <img src={imgURL} alt="Loading..." /> */}
+      <p>Loading...</p>
     </div>
   );
 };
